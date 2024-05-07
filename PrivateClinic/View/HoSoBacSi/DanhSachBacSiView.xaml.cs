1bd7a098d911b25d12f8d587f3881ad9d68d5be4
Vu Binh <22520129@gm.uit.edu.vn>
2024-05-07 23:13:07 +0700

    HoSoBacSi: Hoàn thành chức năng hiển thị danh sách bác sĩ

--- PrivateClinic/View/HoSoBacSi/DanhSachBacSiView.xaml.cs
+++ PrivateClinic/View/HoSoBacSi/DanhSachBacSiView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PrivateClinic.ViewModel.HoSoBacSiVM;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace PrivateClinic.View.HoSoBacSi
         public DanhSachBacSiView()
         {
             InitializeComponent();
+            DanhSachBacSiViewModel viewModel = new DanhSachBacSiViewModel();
+            this.DataContext = viewModel;
         }
     }
 }
---
+++ PrivateClinic/ViewModel/HoSoBacSiVM/DanhSachBacSiViewModel.cs
@@ -0,0 +1,35 @@
+﻿using PrivateClinic.Model;
+using PrivateClinic.ViewModel.OtherViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrivateClinic.ViewModel.HoSoBacSiVM
+{
+    public class DanhSachBacSiViewModel: BaseViewModel
+    {
+        private ObservableCollection<BACSI> dsbs;
+        public ObservableCollection<BACSI> DSBS
+        {
+            get => dsbs;
+            set
+            {
+                dsbs = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DanhSachBacSiViewModel()
+        {
+            DSBS = new ObservableCollection<BACSI>(DataProvider.Ins.DB.BACSIs);
+        }
+
+        void LoadData()
+        {
+            DSBS = new ObservableCollection<BACSI>();
+        }
+    }
+}
