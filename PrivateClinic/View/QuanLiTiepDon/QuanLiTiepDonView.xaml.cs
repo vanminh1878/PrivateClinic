04eaee0cb23c33c5271429221a0ee006fd805308
Lương Văn Minh <147115879+vanminh1878@users.noreply.github.com>
2024-05-08 22:58:19 +0700

    Man hinh QuanLiTiepDon Done backend

--- PrivateClinic/View/QuanLiTiepDon/QuanLiTiepDonView.xaml.cs
+++ PrivateClinic/View/QuanLiTiepDon/QuanLiTiepDonView.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PrivateClinic.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static MaterialDesignThemes.Wpf.Theme;
+using Button = System.Windows.Controls.Button;
+using PrivateClinic.ViewModel.QuanLiTiepDon;
 
 namespace PrivateClinic.View.QuanLiTiepDon
 {
@@ -23,6 +28,38 @@ namespace PrivateClinic.View.QuanLiTiepDon
         public QuanLiTiepDonView()
         {
             InitializeComponent();
+            QuanLiTiepDonViewModel viewmodel = new QuanLiTiepDonViewModel();
+            this.DataContext = viewmodel;
+
         }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is QuanLiTiepDonViewModel viewModel)
+            {
+                var selectedBenhNhan = (sender as Button)?.CommandParameter as BENHNHAN;
+
+                if (selectedBenhNhan != null)
+                {
+                    viewModel.DeleteCommand.Execute(selectedBenhNhan);
+                    ListViewBN.Items.Refresh();
+                }
+            }
+        }
+
+        private void EditBNButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is QuanLiTiepDonViewModel viewModel)
+            {
+                var selectedBenhNhan = (sender as Button)?.CommandParameter as BENHNHAN;
+
+                if (selectedBenhNhan != null)
+                {
+                    viewModel.EditBNCommand.Execute(selectedBenhNhan);
+                    ListViewBN.Items.Refresh();
+                }
+            }
+        }
+
     }
 }
