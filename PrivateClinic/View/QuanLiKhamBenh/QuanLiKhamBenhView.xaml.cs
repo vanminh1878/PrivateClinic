670a6f6e80d101054718a6ad1f0c99cd4d4b39a7
Vu Binh <22520129@gm.uit.edu.vn>
2024-05-08 13:36:13 +0700

    QuanLiKhamBenh: Tạo chuyển hướng cho các UserControl

--- PrivateClinic/View/QuanLiKhamBenh/QuanLiKhamBenhView.xaml.cs
+++ PrivateClinic/View/QuanLiKhamBenh/QuanLiKhamBenhView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PrivateClinic.ViewModel.QuanLiKhamBenhVM;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,9 @@ namespace PrivateClinic.View.QuanLiKhamBenh
         public QuanLiKhamBenhView()
         {
             InitializeComponent();
+            Main.Content = new BenhNhanDaKhamView();
+            QuanLiKhamBenhViewModel viewModel = new QuanLiKhamBenhViewModel();
+            this.DataContext = viewModel;
         }
     }
 }
---
+++ PrivateClinic/ViewModel/QuanLiKhamBenhVM/QuanLiKhamBenhViewModel.cs
@@ -0,0 +1,43 @@
+﻿using PrivateClinic.View.QuanLiKhamBenh;
+using PrivateClinic.ViewModel.OtherViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PrivateClinic.ViewModel.QuanLiKhamBenhVM
+{
+    public class QuanLiKhamBenhViewModel : BaseViewModel
+    {
+        private object currentView;
+        public object CurrentView
+        {
+            get => currentView;
+            set
+            {
+                currentView = value;
+                OnPropertyChanged(nameof(CurrentView));
+            }
+        }
+
+        public ICommand SwitchViewCommand { get; set; }
+
+        public QuanLiKhamBenhViewModel()
+        {
+            SwitchViewCommand = new ViewModelCommand(SwitchView);
+        }
+
+        private void SwitchView(object userControlName)
+        {
+            string UserControlName = userControlName as string;
+            switch (UserControlName)
+            {
+                case "PatientList":
+                    CurrentView = new BenhNhanDaKhamView();
+                    break;
+            }
+        }
+    }
+}
