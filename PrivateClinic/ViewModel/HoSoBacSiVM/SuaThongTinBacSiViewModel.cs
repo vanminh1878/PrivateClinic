a07105c0ecd418d969f17f516ff33e18051e58f8
Vu Binh <22520129@gm.uit.edu.vn>
2024-05-14 23:46:49 +0700

    HoSoBacSi: Hoàn thành nút hủy cho màn hình sửa thông tin bác sĩ

---
+++ PrivateClinic/ViewModel/HoSoBacSiVM/SuaThongTinBacSiViewModel.cs
@@ -0,0 +1,27 @@
+﻿using PrivateClinic.View.HoSoBacSi;
+using PrivateClinic.ViewModel.OtherViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PrivateClinic.ViewModel.HoSoBacSiVM
+{
+    public class SuaThongTinBacSiViewModel : BaseViewModel
+    {
+        private SuaThongTinBacSiView _view;
+        public SuaThongTinBacSiViewModel(SuaThongTinBacSiView view) 
+        {
+            this._view = view;
+            CancelCommand = new ViewModelCommand(quit);
+
+        }
+        public ICommand CancelCommand { get; set; }
+        private void quit(object obj)
+        {
+            _view.Close();
+        }
+    }
+}
