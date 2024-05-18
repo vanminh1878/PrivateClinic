7704cffe4d6ffbaa5c00f4c59cc524f29d8bc34f
Vu Binh <22520129@gm.uit.edu.vn>
2024-05-18 23:42:25 +0700

    QuanLiBenhNhan: Hoàn thành đẩy dữ liệu lên listview

--- PrivateClinic/View/QuanLiKhamBenh/BenhNhanDaKhamView.xaml.cs
+++ PrivateClinic/View/QuanLiKhamBenh/BenhNhanDaKhamView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PrivateClinic.ViewModel.QuanLiKhamBenhVM;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace PrivateClinic.View.QuanLiKhamBenh
         public BenhNhanDaKhamView()
         {
             InitializeComponent();
+            BenhNhanDaKhamViewModel viewModel = new BenhNhanDaKhamViewModel();
+            this.DataContext = viewModel;
         }
     }
 }
---
+++ PrivateClinic/ViewModel/QuanLiKhamBenhVM/BenhNhanDaKhamViewModel.cs
@@ -0,0 +1,106 @@
+﻿using PrivateClinic.Model;
+using PrivateClinic.ViewModel.OtherViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrivateClinic.ViewModel.QuanLiKhamBenhVM
+{
+    public class BenhNhanDaKhamViewModel : BaseViewModel
+    {
+        #region Các Command và Property
+        private ObservableCollection<BenhNhanDTO> listBN;
+        public ObservableCollection<BenhNhanDTO> ListBN
+        {
+            get => listBN; 
+            set
+            {
+                listBN = value;
+                OnPropertyChanged(nameof(ListBN));
+            }
+        }
+        private ObservableCollection<BENHNHAN> benhnhan;
+        public ObservableCollection<BENHNHAN> BenhNhan
+        {
+            get => benhnhan;
+            set
+            {
+                benhnhan = value;
+                OnPropertyChanged(nameof(BenhNhan));
+            }
+        }
+
+        private ObservableCollection<PHIEUKHAMBENH> listpkb;
+        public ObservableCollection<PHIEUKHAMBENH> ListPKB
+        {
+            get => listpkb;
+            set
+            {
+                listpkb = value;
+                OnPropertyChanged(nameof(ListPKB));
+            }
+        }
+
+        private ObservableCollection<LOAIBENH> listLoaiBenh;
+        public ObservableCollection<LOAIBENH> ListLoaiBenh
+        {
+            get => listLoaiBenh;
+            set
+            {
+                listLoaiBenh = value;
+                OnPropertyChanged(nameof(ListLoaiBenh));
+            }
+        }
+        #endregion
+
+        //Hàm khởi tạo
+        public BenhNhanDaKhamViewModel()
+        {
+            LoadData();
+        }
+
+        //Hàm load dữ liệu lên list view 
+        void LoadData()
+        {
+            // Lấy tất cả phiếu khám bệnh
+            ListPKB = new ObservableCollection<PHIEUKHAMBENH>(DataProvider.Ins.DB.PHIEUKHAMBENH);
+            // Lấy tất cả loại bệnh
+            ListLoaiBenh = new ObservableCollection<LOAIBENH>(DataProvider.Ins.DB.LOAIBENH);
+            // Lấy tất cả bệnh nhân
+            BenhNhan = new ObservableCollection<BENHNHAN>(DataProvider.Ins.DB.BENHNHAN);
+
+            // Tạo từ điển để tra cứu nhanh
+            var pkbDictionary = ListPKB.GroupBy(pkb => pkb.MaBN).ToDictionary(group => group.Key, group => group.ToList());
+            var loaiBenhDictionary = ListLoaiBenh.ToDictionary(lb => lb.MaLoaiBenh, lb => lb.TenLoaiBenh);
+
+            // Xử lý
+            ListBN = new ObservableCollection<BenhNhanDTO>();
+            int stt = 1;
+
+            foreach (var benhnhan in BenhNhan)
+            {
+                if (pkbDictionary.TryGetValue(benhnhan.MaBN, out var pkbList))
+                {
+                    foreach (var pkb in pkbList)
+                    {
+                        BenhNhanDTO benhNhanDTO = new BenhNhanDTO
+                        {
+                            STT = stt,
+                            HoTen = benhnhan.HoTen,
+                            TrieuChung = pkb.TrieuChung,
+                            NgayKham = pkb.NgayKham,
+                            TenLoaiBenh = loaiBenhDictionary.TryGetValue(pkb.MaLoaiBenh, out var tenLoaiBenh) ? tenLoaiBenh : string.Empty
+                        };
+
+                        ListBN.Add(benhNhanDTO);
+                        stt++;
+                    }
+                }
+            }
+        }
+
+    }
+}
