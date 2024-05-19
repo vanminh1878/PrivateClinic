b9b4b5a3a4324da314dbd414c73f12fc51008969
Vu Binh <22520129@gm.uit.edu.vn>
2024-05-19 22:41:43 +0700

    HoSoBacSi + QuanLiBenhNhan: Hoàn thành việc hiển thị thông tin user

--- PrivateClinic/ViewModel/HoSoBacSiVM/HoSoBacSiViewModel.cs
+++ PrivateClinic/ViewModel/HoSoBacSiVM/HoSoBacSiViewModel.cs
@@ -1,17 +1,24 @@
-﻿using PrivateClinic.View.HoSoBacSi;
+﻿using PrivateClinic.Model;
+using PrivateClinic.View.HoSoBacSi;
 using PrivateClinic.ViewModel.OtherViewModels;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows.Resources;
 using System.Xml.Linq;
 
 namespace PrivateClinic.ViewModel.HoSoBacSiVM
 {
     public class HoSoBacSiViewModel : BaseViewModel
     {
+        #region Các Property và Command
         private object currentView;
         public object CurrentView
         {
@@ -25,10 +32,52 @@ namespace PrivateClinic.ViewModel.HoSoBacSiVM
                 OnPropertyChanged(nameof(CurrentView));
             }
         }
+        private string chucvu;
+        public string ChucVu
+        {
+            get => chucvu;
+            set
+            {
+                chucvu = value;
+                OnPropertyChanged(nameof(ChucVu));
+            }
+        }
+        private string tenNV;
+        public string TenNV
+        {
+            get => tenNV;
+            set
+            {
+                tenNV = value;
+                OnPropertyChanged(nameof(TenNV));
+            }
+        }
+        private NGUOIDUNG user;
+        public NGUOIDUNG User
+        {
+            get => user;
+            set
+            {
+                user = value;
+                OnPropertyChanged(nameof(User));
+            }
+        }
+        private BitmapImage imageSource;
+        public BitmapImage ImageSource
+        {
+            get => imageSource;
+            set
+            {
+                imageSource = value;
+                OnPropertyChanged(nameof(ImageSource));
+            }
+        }
         public ICommand SwitchViewCommand { get; private set; }
+        #endregion
         public HoSoBacSiViewModel()
         {
             SwitchViewCommand = new ViewModelCommand(SwitchView);
+            ThongTinND();
         }
         private void SwitchView(object userControlName)
         {
@@ -46,6 +95,46 @@ namespace PrivateClinic.ViewModel.HoSoBacSiVM
                     break;
 
 
+            }
+        }
+        //Hàm load thông tin người dùng và ảnh
+        private void ThongTinND()
+        {
+            string tendangnhap = Const.TenDangNhap;
+            User = DataProvider.Ins.DB.NGUOIDUNG.Where(x => x.TenDangNhap == tendangnhap).FirstOrDefault();
+            string MaBS = User.MaBS.ToString();
+            ObservableCollection<BACSI> DSBS = new ObservableCollection<BACSI>(DataProvider.Ins.DB.BACSI);
+            if (Const.PQ.MaNhom == "NHOM1")
+                ChucVu = "Admin";
+            else
+                ChucVu = "Nhân viên";
+            foreach (BACSI bs in DSBS)
+            {
+                if (bs.MaBS.ToString() == MaBS)
+                {
+                    TenNV = bs.HoTen;
+                    if (bs.Image == null)
+                    {
+                        Uri resourceUri = new Uri("pack://application:,,,/ResourceXAML/image/ic_male_user_blue.png", UriKind.Absolute);
+                        StreamResourceInfo streamInfo = System.Windows.Application.GetResourceStream(resourceUri);
+                        byte[] imageBytes;
+                        using (Stream imageStream = streamInfo.Stream)
+                        {
+                            using (MemoryStream ms = new MemoryStream())
+                            {
+                                imageStream.CopyTo(ms);
+                                imageBytes = ms.ToArray();
+                            }
+                        }
+                        ImageSource = ImageViewModel.ByteArrayToBitmapImage(imageBytes);
+                    }
+                    else
+                    {
+                        BitmapImage image = ImageViewModel.ByteArrayToBitmapImage(bs.Image);
+                        ImageSource = image;
+                    }
+                    break;
+                }
             }
         }
     }
--- PrivateClinic/ViewModel/QuanLiKhamBenhVM/QuanLiKhamBenhViewModel.cs
+++ PrivateClinic/ViewModel/QuanLiKhamBenhVM/QuanLiKhamBenhViewModel.cs
@@ -1,11 +1,16 @@
-﻿using PrivateClinic.View.QuanLiKhamBenh;
+﻿using PrivateClinic.Model;
+using PrivateClinic.View.QuanLiKhamBenh;
 using PrivateClinic.ViewModel.OtherViewModels;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Resources;
 
 namespace PrivateClinic.ViewModel.QuanLiKhamBenhVM
 {
@@ -22,12 +27,53 @@ namespace PrivateClinic.ViewModel.QuanLiKhamBenhVM
                 OnPropertyChanged(nameof(CurrentView));
             }
         }
+
+        private string chucvu;
+        public  string ChucVu { 
+            get =>chucvu;
+            set
+            {
+                chucvu = value;
+                OnPropertyChanged(nameof(ChucVu));
+            }
+        }
+        private string tenNV;
+        public string TenNV
+        {
+            get => tenNV;
+            set
+            {
+                tenNV = value;
+                OnPropertyChanged(nameof(TenNV));
+            }
+        }
+        private NGUOIDUNG user;
+        public NGUOIDUNG User
+        {
+            get => user;
+            set
+            {
+                user = value;
+                OnPropertyChanged(nameof(User));
+            }
+        }
+        private BitmapImage imageSource;
+        public BitmapImage ImageSource
+        {
+            get => imageSource;
+            set
+            {
+                imageSource = value;
+                OnPropertyChanged(nameof(ImageSource));
+            }
+        }
         public ICommand SwitchViewCommand { get; set; }
         #endregion
         //Hàm khởi tạo
         public QuanLiKhamBenhViewModel()
         {
             SwitchViewCommand = new ViewModelCommand(SwitchView);
+            ThongTinND();
         }
 
         //Hàm chuyển màn hình
@@ -39,6 +85,47 @@ namespace PrivateClinic.ViewModel.QuanLiKhamBenhVM
                 case "PatientList":
                     CurrentView = new BenhNhanDaKhamView();
                     break;
+            }
+        }
+
+        //Hàm load thông tin người dùng và ảnh
+        private void ThongTinND()
+        {
+            string tendangnhap = Const.TenDangNhap;
+            User = DataProvider.Ins.DB.NGUOIDUNG.Where(x => x.TenDangNhap == tendangnhap).FirstOrDefault();
+            string MaBS = User.MaBS.ToString();
+            ObservableCollection<BACSI>DSBS = new ObservableCollection<BACSI>(DataProvider.Ins.DB.BACSI);
+            if (Const.PQ.MaNhom == "NHOM1")
+                ChucVu = "Admin";
+            else
+                ChucVu = "Nhân viên";
+            foreach (BACSI bs in DSBS) 
+            {
+                if (bs.MaBS.ToString() == MaBS) 
+                {
+                    TenNV = bs.HoTen;
+                    if (bs.Image == null)
+                    {
+                        Uri resourceUri = new Uri("pack://application:,,,/ResourceXAML/image/ic_male_user_blue.png", UriKind.Absolute);
+                        StreamResourceInfo streamInfo = System.Windows.Application.GetResourceStream(resourceUri);
+                        byte[] imageBytes;
+                        using (Stream imageStream = streamInfo.Stream)
+                        {
+                            using (MemoryStream ms = new MemoryStream())
+                            {
+                                imageStream.CopyTo(ms);
+                                imageBytes = ms.ToArray();
+                            }
+                        }
+                        ImageSource = ImageViewModel.ByteArrayToBitmapImage(imageBytes);
+                    }
+                    else
+                    {
+                        BitmapImage image = ImageViewModel.ByteArrayToBitmapImage(bs.Image);
+                        ImageSource = image;
+                    }
+                    break;
+                }
             }
         }
     }
