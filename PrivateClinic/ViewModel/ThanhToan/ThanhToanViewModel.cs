8ff0c7f2f828a21a3d11e6fdb9aa4b8a293175fc
Vu Binh <22520129@gm.uit.edu.vn>
2024-05-25 00:03:21 +0700

    Cập nhật ảnh và tên cho màn hình thanh toán

--- PrivateClinic/ViewModel/ThanhToan/ThanhToanViewModel.cs
+++ PrivateClinic/ViewModel/ThanhToan/ThanhToanViewModel.cs
@@ -5,6 +5,10 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows.Input;
 using System.Windows;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Windows.Resources;
+using System;
 
 namespace PrivateClinic.ViewModel.ThanhToan
 {
@@ -69,11 +73,51 @@ namespace PrivateClinic.ViewModel.ThanhToan
             Unpaid = 0,
             Paid = 1
         }
-
+        private string chucvu;
+        public string ChucVu
+        {
+            get => chucvu;
+            set
+            {
+                chucvu = value;
+                OnPropertyChanged(nameof(ChucVu));
+            }
+        }
+        private string tenNV;
+        public string TenNV
+        {
+            get => tenNV;
+            set
+            {
+                tenNV = value;
+                OnPropertyChanged(nameof(TenNV));
+            }
+        }
+        private NGUOIDUNG user;
+        public NGUOIDUNG User
+        {
+            get => user;
+            set
+            {
+                user = value;
+                OnPropertyChanged(nameof(User));
+            }
+        }
+        private BitmapImage imageSource;
+        public BitmapImage ImageSource
+        {
+            get => imageSource;
+            set
+            {
+                imageSource = value;
+                OnPropertyChanged(nameof(ImageSource));
+            }
+        }
         #endregion
 
         public ThanhToanViewModel()
         {
+            ThongTinND();
             listHD = new ObservableCollection<HOADON>(DataProvider.Ins.DB.HOADONs);
             SearchCommand = new RelayCommand<ThanhToanView>((p) => { return p == null ? false : true; }, (p) => _SearchCommand(p));
             PayHDCommand = new RelayCommand<HOADON>((p) => { return p == null ? false : true; }, (p) => _PayHDCommand(p));
@@ -175,6 +219,46 @@ namespace PrivateClinic.ViewModel.ThanhToan
                 if (hoaDonView.DialogResult == true)
                 {
                     this.RefreshData();
+                }
+            }
+        }
+        //Hàm load thông tin người dùng và ảnh
+        private void ThongTinND()
+        {
+            string tendangnhap = Const.TenDangNhap;
+            User = DataProvider.Ins.DB.NGUOIDUNGs.Where(x => x.TenDangNhap == tendangnhap).FirstOrDefault();
+            string MaBS = User.MaBS.ToString();
+            ObservableCollection<BACSI> DSBS = new ObservableCollection<BACSI>(DataProvider.Ins.DB.BACSIs);
+            if (Const.PQ.MaNhom == "NHOM1")
+                ChucVu = "Admin";
+            else
+                ChucVu = "Nhân viên";
+            foreach (BACSI bs in DSBS)
+            {
+                if (bs.MaBS.ToString() == MaBS)
+                {
+                    TenNV = bs.HoTen;
+                    if (bs.Image == null)
+                    {
+                        Uri resourceUri = new Uri("pack://application:,,,/ResourceXAML/image/ic_male_user_blue.png", UriKind.Absolute);
+                        StreamResourceInfo streamInfo = System.Windows.Application.GetResourceStream(resourceUri);
+                        byte[] imageBytes;
+                        using (Stream imageStream = streamInfo.Stream)
+                        {
+                            using (MemoryStream ms = new MemoryStream())
+                            {
+                                imageStream.CopyTo(ms);
+                                imageBytes = ms.ToArray();
+                            }
+                        }
+                        ImageSource = ImageViewModel.ByteArrayToBitmapImage(imageBytes);
+                    }
+                    else
+                    {
+                        BitmapImage image = ImageViewModel.ByteArrayToBitmapImage(bs.Image);
+                        ImageSource = image;
+                    }
+                    break;
                 }
             }
         }
