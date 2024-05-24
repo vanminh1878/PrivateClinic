e621faf4e2ec446eda65f631140b3783f2fb5e72
Saidia <20520318@gm.uit.edu.vn>
2024-05-24 22:29:15 +0700

    Reapply "Merge pull request #3 from vanminh1878/dev_tinh"
    This reverts commit 39ba9c83a67305b1de63e10ab1df7c8907afbb90.

---
+++ PrivateClinic/Converters/AgeConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace PrivateClinic.Converters
+{
+    public class AgeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string yearOfBirthString)
+            {
+                if (int.TryParse(yearOfBirthString, out int yearOfBirth))
+                {
+                    int currentYear = DateTime.Now.Year;
+                    int age = currentYear - yearOfBirth;
+                    return age;
+                }
+            }
+            return "Unknown";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("Converting from age to year of birth is not supported.");
+        }
+    }
+}
---
+++ PrivateClinic/Converters/EnumToIntConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PrivateClinic.Converters
+{
+    public class EnumToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Enum)
+            {
+                return (int)value;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                try
+                {
+                    return Enum.Parse(targetType, value.ToString());
+                }
+                catch
+                {
+                    return DependencyProperty.UnsetValue;
+                }
+            }
+            return DependencyProperty.UnsetValue;
+        }
+    }
+
+}
