f15951a2283c46b7a280bdf10eea6599f7afc48d
Vu Binh <22520129@gm.uit.edu.vn>
2024-05-24 12:42:57 +0700

    ViewModelCommand: Thêm file

--- PrivateClinic/ViewModel/OtherViewModels/ViewModelCommand.cs
+++ PrivateClinic/ViewModel/OtherViewModels/ViewModelCommand.cs
@@ -7,43 +7,43 @@ using System.Windows.Input;
 
 namespace PrivateClinic.ViewModel.OtherViewModels
 {
-    public class ViewModelCommand : ICommand
-    {
-        // Fields
-        private readonly Action<object> _executeAction;
-        private readonly Predicate<object> _canExecuteAction;
-
-        //Contructors
-        public ViewModelCommand(Action<object> executeAction, Predicate<object> canExecuteAction)
+        public class ViewModelCommand : ICommand
         {
-            _executeAction = executeAction;
-            _canExecuteAction = canExecuteAction;
+            // Fields
+            private readonly Action<object> _executeAction;
+            private readonly Predicate<object> _canExecuteAction;
+
+            //Contructors
+            public ViewModelCommand(Action<object> executeAction, Predicate<object> canExecuteAction)
+            {
+                _executeAction = executeAction;
+                _canExecuteAction = canExecuteAction;
+            }
+
+            public ViewModelCommand(Action<object> executeAction)
+            {
+                _executeAction = executeAction;
+                _canExecuteAction = null;
+            }
+
+            // Events
+
+            public event EventHandler CanExecuteChanged
+            {
+                add { CommandManager.RequerySuggested += value; }
+                remove { CommandManager.RequerySuggested -= value; }
+            }
+
+            // Methods
+            public bool CanExecute(object parameter)
+            {
+                return _canExecuteAction == null ? true : _canExecuteAction(parameter);
+            }
+
+            public void Execute(object parameter)
+            {
+                _executeAction(parameter);
+            }
+
         }
-
-        public ViewModelCommand(Action<object> executeAction)
-        {
-            _executeAction = executeAction;
-            _canExecuteAction = null;
-        }
-
-        // Events
-
-        public event EventHandler CanExecuteChanged
-        {
-            add { CommandManager.RequerySuggested += value; }
-            remove { CommandManager.RequerySuggested -= value; }
-        }
-
-        // Methods
-        public bool CanExecute(object parameter)
-        {
-            return _canExecuteAction == null ? true : _canExecuteAction(parameter);
-        }
-
-        public void Execute(object parameter)
-        {
-            _executeAction(parameter);
-        }
-
-    }
 }
